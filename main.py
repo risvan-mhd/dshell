55f197adff5900fe00598c86b7013c55c416e22f
Risvan Muhammad <risvan145k@gmail.com>
2026-06-19 19:54:12 +0530

    Implement basic shell

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from pathlib import Path
+import cmd
+
+
+ESC = "\033["
+BLUE = f"{ESC}34m"
+RESET = f"{ESC}0m"
+
+
+class Shell(cmd.Cmd):
+    @property
+    def prompt(self) -> str:  # type: ignore
+        return f"{BLUE}{Path.cwd().name}{RESET} $ "
+
+
 def main() -> None:
-    print("Hello, World!")
+    try:
+        Shell().cmdloop()
+    except KeyboardInterrupt:
+        print("\nquit")
 
 
 if __name__ == "__main__":
